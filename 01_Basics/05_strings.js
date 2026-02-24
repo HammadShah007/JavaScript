2063be42c708d9c2ddc2a5d1fdf0d07d6c8a2c48
Muhammad Hammad <hammad.shaha007@gmail.com>
2026-02-24 21:24:28 +0500

    learing string and it's methods

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "Hammad";
+
+const age = 22;
+
+// console.log(name + age);
+
+console.log(`Salam my name is ${name} and my age is ${age}`);
+
+// let gameName = new String('Call of duty');
+
+let gameName = "Call of duty";
+
+// console.log(gameName.toUpperCase());
+
+
+
