02981a065f095273419bd88e6bf74d52b6f85a17
Muhammad Hammad <hammad.shaha007@gmail.com>
2026-03-07 21:15:26 +0500

    learn Dates

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,26 @@
+//Dates
+
+const now = new Date();
+
+// console.log(typeof now);
+
+// console.log(now);
+
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getFullYear());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+
+console.log(now.toLocaleString());
+console.log(now.toDateString());
+console.log(now.toTimeString());
+
+const myCreatedDate = new Date(2033, 0, 22);
+
+console.log(myCreatedDate);
+
+
+
+
