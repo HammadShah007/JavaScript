a7bd6f357f1072edfd76bed012b2b8b29246454c
Muhammad Hammad <hammad.shaha007@gmail.com>
2026-03-07 21:21:42 +0500

    learn Dates more

--- 01_Basics/07_dates.js
+++ 01_Basics/07_dates.js
@@ -13,13 +13,38 @@ const now = new Date();
 // console.log(now.getMinutes());
 // console.log(now.getSeconds());
 
-console.log(now.toLocaleString());
-console.log(now.toDateString());
-console.log(now.toTimeString());
+// console.log(now.toLocaleString());
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
 
-const myCreatedDate = new Date(2033, 0, 22);
+// const myCreatedDate = new Date(2033, 0, 22);
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
 
-console.log(myCreatedDate);
+// console.log(myCreatedDate);
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+
+const myDate = new Date(myTimeStamp);
+
+console.log(myDate);
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
 
 
 
