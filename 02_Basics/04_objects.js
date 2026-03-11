704d1a81848d856e4c5d653dec1349bc6eecc8af
Muhammad Hammad <hammad.shaha007@gmail.com>
2026-03-11 21:28:26 +0500

    learn object more n more

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -27,5 +27,54 @@ const obj2 = {3: "c", 4: "d"};
 // const obj3 = Object.assign({}, obj1, obj2)
 const obj3 = {...obj1, ...obj2}
 
-console.log(obj3);
+// console.log(obj3);
+
+
+const users = [
+    {
+        userId: 1, 
+        name: "Hammad",
+        isLoggedIn: false
+    },
+    {
+        userId: 2, 
+        name: "Hassan",
+        isLoggedIn: false
+    },
+    {
+        userId: 3, 
+        name: "Hunain",
+        isLoggedIn: false
+    },
+    {
+        userId: 4, 
+        name: "Hussain",
+        isLoggedIn: false
+    },
+    {
+        userId: 5, 
+        name: "Hasnain",
+        isLoggedIn: false
+    },
+    {
+        userId: 6, 
+        name: "Ali",
+        isLoggedIn: false
+    },
+]
+
+// console.log(users[0].name);
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
+
+
+
 
