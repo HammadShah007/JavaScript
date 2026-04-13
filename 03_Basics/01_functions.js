93f257cc309a26fcb820dcfe2ef57bbdedbc03e6
Muhammad Hammad <hammad.shaha007@gmail.com>
2026-04-13 20:25:03 +0500

    learn scope, this, arrow function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -8,7 +8,7 @@
 // }
 
 // console.log(sayMyName); //Referring to the function
-// sayMyName(); // calling the function
+// sayMyName(); // calling the function or execute the function
 
 //Note: If you forget the parentheses, the function will not be executed, and instead, it will return the function definition.
 
@@ -25,28 +25,41 @@
 // console.log("Result: ", result); // Output: undefined
 
 function addTwoNumbers(num1, num2) {
-    // const sum = num1 + num2;
-    // console.log("The sum is: ", sum); // This will print the sum of num1 and num2 to the console.
-    // return sum; // The return statement is used to specify the value that a function should return when it is called. In this case, the function will return the value of sum, which is the result of adding num1 and num2.
-    // console.log("This will not be executed"); // Any code after the return statement will not be executed, as the function will exit immediately after returning the value.
-    return num1 + num2; // We can directly return the result of the addition without storing it in a variable.
+  // const sum = num1 + num2;
+  // console.log("The sum is: ", sum); // This will print the sum of num1 and num2 to the console.
+  // return sum; // The return statement is used to specify the value that a function should return when it is called. In this case, the function will return the value of sum, which is the result of adding num1 and num2.
+  // console.log("This will not be executed"); // Any code after the return statement will not be executed, as the function will exit immediately after returning the value.
+  return num1 + num2; // We can directly return the result of the addition without storing it in a variable.
 }
 
 // const result = addTwoNumbers(7, 8); // Now the function will return the sum of 7 and 8, which is 15.
 // console.log("Result: ", result); // Output: Result: 15
 
-
 function loginUserMessage(username) {
-    if (username === undefined) {
-        console.log("Please enter a username");
-        return; // This will exit the function if the username is not provided. but it will not work if the username is an empty string, as an empty string is a valid value for a username. To handle this case, we can check if the username is falsy (i.e., if it is an empty string, null, undefined, 0, or false) instead of checking if it is undefined.
-    }
-    // if (!username) {
-    //     console.log("Please enter a username");
-    //     return; // This will exit the function if the username is not provided.
-    // }
-    return `Welcome back, ${username}!`; // This will return a welcome message with the username.
+  if (username === undefined) {
+    console.log("Please enter a username");
+    return; // This will exit the function if the username is not provided. but it will not work if the username is an empty string, as an empty string is a valid value for a username. To handle this case, we can check if the username is falsy (i.e., if it is an empty string, null, undefined, 0, or false) instead of checking if it is undefined.
+  }
+  // if (!username) {
+  //     console.log("Please enter a username");
+  //     return; // This will exit the function if the username is not provided.
+  // }
+  return `Welcome back, ${username}!`; // This will return a welcome message with the username.
+  // after return statement, the function will exit and any code after the return statement will not be executed. So, if we try to log a message after the return statement, it will not be executed.
 }
 
-// console.log(loginUserMessage());
+console.log(loginUserMessage());
 // console.log(loginUserMessage(""));
+
+function calculateBill(val1, val2, ...amount) {
+  return amount;
+}
+
+// console.log(calculateBill(200, 300, 400, 500));
+
+const user = {
+    userName: "Hammad",
+    price: 199
+}
+
+
---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,13 @@
+//  Normal Function
+
+// function Myiife(params) {
+//     console.log(`DB Connected`);
+// }
+    
+// Myiife()
+    
+// Immediately Invoked Function Expressions (IIFE)
+
+(function Myiife(params) {
+    console.log(`DB Connected`);
+})()
